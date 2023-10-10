f7cf1a2c1a3141a0ed2f3dea750f9382fc99957f
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-10 19:02:53 +0330

    Change order of test section

--- tests/test_search.py
+++ tests/test_search.py
@@ -9,21 +9,18 @@ from pages.search import DuckDuckGoSearchPage
 def test_duck_duck_go_search(
     search_page: DuckDuckGoSearchPage, result_page: DuckDuckGoResultPage
 ):
-    # Arrange Section
+    # Given duckduckgo page is displayed
     url = 'https://www.duckduckgo.com'
     phrase = 'Alireza Soltani Jazi'
-
-    # Act Section
-    # Give duckduckgo page is displayed
     search_page.load_search_page(url)
+
     # When the user searches for a "word"
     search_page.search_value(phrase)
+
     # Then the result are shown for the "word"
     link_titles = result_page.get_link_titles()
     search_input_text = result_page.get_search_input_text()
     page_title = result_page.get_page_title()
-
-    # Assert Section
     assert_that(any(phrase in title for title in link_titles), 'link titles').is_true()
     assert_that(search_input_text, 'search input text').is_equal_to(phrase)
     assert_that(page_title, 'page title').starts_with(phrase)
